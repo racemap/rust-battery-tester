a0f9e09d93ab5bbdacc84856cb82a3309427f408
alex <alexander.lindner1@mailbox.tu-dresden.de>
2024-08-21 11:28:29 +0200

    added webserver
    added an easy implemntaion of a webserver which should later provide the data

--- src/network.rs
+++ src/network.rs
@@ -1,7 +1,11 @@
 use embassy_executor::Spawner;
+use embedded_svc::http::Method;
 use embedded_svc::utils::asyncify::Asyncify;
 use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
+use esp_idf_svc::http::server::{Configuration as HttpServerConfig, EspHttpServer};
 use esp_idf_svc::wifi::WifiEvent;
+
+use embassy_time::{Duration, Timer};
 use esp_idf_svc::{eventloop::EspSystemEventLoop, wifi::EspWifi};
 use log::*;
 
@@ -28,8 +32,9 @@ pub async fn init_network(
     wifi.start().ok();
 
     wifi.connect().ok();
-
+    println!("IP info: {:?}", wifi.sta_netif().get_ip_info().unwrap());
     spawner.spawn(wifi_event_handler(wifi, sysloop)).ok();
+    spawner.spawn(webserver()).ok();
 }
 
 #[embassy_executor::task]
@@ -57,4 +62,63 @@ pub async fn wifi_event_handler(mut wifi: EspWifi<'static>, mut sysloop: EspSyst
             _ => {}
         }
     }
+}
+
+#[embassy_executor::task]
+pub async fn webserver() {
+    let mut httpserver = match EspHttpServer::new(&HttpServerConfig::default()) {
+        Ok(ser) => ser,
+        Err(e) => {
+            warn!("unable to start webserver {}", e);
+            return;
+        }
+    };
+
+    httpserver.fn_handler("/", Method::Get, |request| {
+        // Retrieve html String
+        let html = index_html();
+        // Respond with OK status
+        let mut response = request.into_ok_response()?;
+        // Return Requested Object (Index Page)
+        response.write(html.as_bytes())?;
+        Ok(())
+    });
+    loop {
+        Timer::after(Duration::from_secs(1)).await;
+    }
+}
+
+fn index_html() -> String {
+    format!(
+        r#"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://unpkg.com/htmx.org@2.0.2"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+</head>
+<body>
+<header data-bs-theme="dark">
+  <div class="navbar navbar-dark bg-dark shadow-sm">
+    <div class="container">
+      <a class="navbar-brand d-flex align-items-center">
+        <strong>Battery Health check</strong>
+      </a>
+    </div>
+  </div>
+</header>
+<main>
+<section class="py-5 text-center container">
+</section>
+</main>
+</body>
+</html>
+"#
+    )
 }
